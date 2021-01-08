efd119d72c2479ddab4f6e54c54cf839457305db
sevenseasofbri <vishruti250102@gmail.com>
2021-01-08 16:42:26 +0800

    use meme generator api

--- popup.js
+++ popup.js
@@ -1,48 +1,26 @@
-function show_image(src, width, height, alt) {
-    var img = document.createElement("img");
-    img.src = src;
-    img.width = width;
-    img.height = height;
-    img.alt = alt;
-
-    // This next line will just add it to the <body> tag
-    document.body.appendChild(img);
+function show_image(width, height, alt) {
+    var jqxhr = $.getJSON( "https://meme-api.herokuapp.com/gimme", function() {
+        console.log( "success" );
+      })
+        .done(function() {
+          console.log( "second success" );
+          console.log(jqxhr.responseJSON.url);
+        })
+        .fail(function() {
+          console.log( "error" );
+        })
+        .always(function() {
+            console.log( "complete" );
+            var img = document.createElement("img");
+            img.src = jqxhr.responseJSON.url;
+            img.width = width;
+            img.height = height;
+            img.alt = alt;
+            //This next line will just add it to the <body> tag
+            document.body.appendChild(img);
+        });
 }
 var checkPageButton = document.getElementById('clickIt');
 checkPageButton.addEventListener('click', function() {
-    show_image('https://i.imgur.com/YmmSEz9.png',  300,   250, 'Big Red Kangaroo')
+    show_image(300,   250, 'Da Meme')
 }, false);
-document.getElementById("btn").addEventListener('click', show_image('https://i.imgur.com/YmmSEz9.png',  300,   250, 'Big Red Kangaroo')); 
-
-
-
-
-// function show_image() {
-//     var jqxhr = $.getJSON( "https://meme-api.herokuapp.com/gimme", function() {
-//         console.log( "success" );
-//       })
-//         .done(function() {
-//           console.log( "second success" );
-//         })
-//         .fail(function() {
-//           console.log( "error" );
-//         })
-//         .always(function() {
-//           console.log( "complete" );
-//         });
-
-//         var str = JSON.stringify(jqxhr);
-//     // This next line will just add it to the <body> tag
-//     // but you can adapt to make it append to the element you want.
-//     document.body.appendChild(jqxhr.url);
-// }
-
-// document.addEventListener('DOMContentLoaded', function() {
-    // var checkPageButton = document.getElementById('clickIt');
-    // checkPageButton.addEventListener('click', function() {
-  
-    //   chrome.tabs.getSelected(null, function(tab) {
-    //     alert("Hello..! It's my first chrome extension.");
-    //   });
-    // }, false);
-//   }, false);
