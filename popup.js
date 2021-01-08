c001ce3e61a0d728347e60c4217bf89240b5ca5d
sevenseasofbri <vishruti250102@gmail.com>
2021-01-09 04:02:18 +0800

    change fonts and css

--- popup.js
+++ popup.js
@@ -1,26 +1,32 @@
-function show_image(width, height, alt) {
-    var jqxhr = $.getJSON( "https://meme-api.herokuapp.com/gimme", function() {
+function show_image(memeQueryUrl, width, height, alt) {
+    var jqxhr = $.getJSON( memeQueryUrl, function() {
         console.log( "success" );
       })
         .done(function() {
           console.log( "second success" );
           console.log(jqxhr.responseJSON.url);
+          var img = document.createElement("img");
+          img.src = jqxhr.responseJSON.url;
+          img.width = width;
+          img.height = height;
+          img.alt = alt;
+          
+          document.body.appendChild(img);
+          var text = document.createElement("small");                   
+          var t = document.createTextNode(`by u/${jqxhr.responseJSON.author} from r/${jqxhr.responseJSON.subreddit}`);      
+          text.appendChild(t);                                          
+          document.getElementById("myDIV").appendChild(text); 
         })
         .fail(function() {
           console.log( "error" );
+          alert("Oops no memes found :(");
         })
         .always(function() {
-            console.log( "complete" );
-            var img = document.createElement("img");
-            img.src = jqxhr.responseJSON.url;
-            img.width = width;
-            img.height = height;
-            img.alt = alt;
-            //This next line will just add it to the <body> tag
-            document.body.appendChild(img);
+            console.log( "complete 2" );
         });
 }
-var checkPageButton = document.getElementById('clickIt');
-checkPageButton.addEventListener('click', function() {
-    show_image(300,   250, 'Da Meme')
+//var checkPageButton = document.getElementById('clickIt');
+document.addEventListener('DOMContentLoaded', function() {
+    let memeQueryUrl = `https://meme-api.herokuapp.com/gimme`;
+    show_image(memeQueryUrl, 300, 250, 'Da Meme');  
 }, false);
